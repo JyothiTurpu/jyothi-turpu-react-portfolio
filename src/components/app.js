0139ea11fe9e682ff659d21ddb69bdee4294b7a0
Jyothi Turpu <jyothi.turpu@gmail.com>
2020-02-22 16:29:56 -0500

    Intro of Props, State & this

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+
+import PortfolioContainer from "./portfolio/portfolio-container";
 
 export default class App extends Component {
     render() {
         return ( 
         <div className = 'app' >
-            <h1>DevCamp React Starter</h1>
-            <h2>React Redux Router</h2>
-            <h3>Jyothi Turpu</h3>
+            <h4>HI There!</h4>
+            <div>
+                {moment().format("MMMM Do YYYY h:mm:ss a")}
+            </div>
+            <PortfolioContainer/>
         </div>
         );
     }
