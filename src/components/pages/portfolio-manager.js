671b5bd8f86edefe5b316a09fe32a1a306f36a11
Jyothi Turpu <jyothi.turpu@gmail.com>
2020-03-18 18:39:53 -0400

    Portfolio SideBar Implemented

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import PortfolioSideBarList from '../portfolio/portfolio-sidebar-list';
+
 
 export default class PortfolioManager extends Component{
   constructor(props) {
     super(props);
+
+    this.state = {
+      portfolioItems: []
+    }
+  }
+
+  getPortfolioItems() {
+    axios.get('https://jyothiturpu.devcamp.space/portfolio/portfolio_items', { withCredentials: true })
+    .then(response => {
+      console.log('Response from PortfolioManager ',response);
+      this.setState({portfolioItems: [...response.data.portfolio_items]});
+    })
+    .catch(error => {
+      console.log('Error in PortfolioManager',error);
+    });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
   }
 
   render() {
     return(
-      <div>
-        <h1>Portfolio Manager</h1>
-        <h1>Portfolio Manager</h1>
+      <div className='portfolio-manager-wrapper'>
+          <div className='left-column'>
+              <h1>Portfolio Manager1</h1>
+          </div>
+
+          <div className='right-column'>
+              <PortfolioSideBarList data={this.state.portfolioItems}/>
+          </div>
+        
       </div>
     );
   }
