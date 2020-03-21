97e6be465f203ccebff5a1d44a16a631dba80c94
Jyothi Turpu <jyothi.turpu@gmail.com>
2020-03-20 20:27:44 -0400

    Added React Drop Zone

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import DropZoneComponent from 'react-dropzone-component'; 
+
+import '../../../node_modules/react-dropzone-component/styles/filepicker.css';
+import '../../../node_modules/dropzone/dist/min/dropzone.min.css';
+
 
 export default class PortfolioForm extends Component {
   constructor(props) {
@@ -10,13 +15,51 @@ export default class PortfolioForm extends Component {
       url: '',
       category: 'eCommerce',
       position: '',
-      thumb_img: '',
-      banner_img: '',
+      thumb_image: '',
+      banner_image: '',
       logo: ''
     }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.componentConfig = this.componentConfig.bind(this);
+    this.djsConfig = this.djsConfig.bind(this);
+    this.handleThumbDrop = this.handleThumbDrop.bind(this);
+    this.handleBannerDrop = this.handleBannerDrop.bind(this);
+    this.handleLogoDrop = this.handleLogoDrop.bind(this);
+  }
+
+  handleThumbDrop() {
+    return ({
+      addedfile: file=>this.setState({thumb_image: file})
+    });
+  }
+
+  handleBannerDrop() {
+    return ({
+      addedfile: file=>this.setState({banner_image: file})
+    });
+  }
+
+  handleLogoDrop() {
+    return ({
+      addedfile: file=>this.setState({logo: file})
+    });
+  }
+
+  componentConfig() {
+    return ({
+      iconFiletypes: ['.jpeg', '.png'],
+      showFiletypeIcon: true,
+      postUrl: 'https://httpbin.org/post' 
+    });
+  }
+
+  djsConfig() {
+    return ({
+      addRemoveLinks: true,
+      maxFiles: 1
+    });
   }
 
   buildForm() {
@@ -27,6 +70,15 @@ export default class PortfolioForm extends Component {
     formData.append('portfolio_item[url]',this.state.url);
     formData.append('portfolio_item[category]',this.state.category);
     formData.append('portfolio_item[position]',this.state.position);
+
+    if(this.state.thumb_image)
+      formData.append('portfolio_item[thumb_image]',this.state.thumb_image);
+
+    if(this.state.banner_image)
+      formData.append('portfolio_item[banner_image]',this.state.banner_image);
+
+    if(this.state.logo)
+      formData.append('portfolio_item[logo]',this.state.logo);
 
     return formData;
   }
@@ -71,6 +123,19 @@ export default class PortfolioForm extends Component {
             </div>
             <div>
                 <textarea type="text" name="description" placeholder='Description' value={this.state.description} onChange={this.handleChange}/>
+            </div>
+            <div className='image-uploaders'>
+                  <DropZoneComponent config={this.componentConfig()} 
+                      djsConfig = {this.djsConfig()}
+                      eventHandlers= {this.handleThumbDrop()}/>
+                  
+                  <DropZoneComponent config={this.componentConfig()} 
+                      djsConfig = {this.djsConfig()}
+                      eventHandlers= {this.handleBannerDrop()}/>
+
+                  <DropZoneComponent config={this.componentConfig()} 
+                      djsConfig = {this.djsConfig()}
+                      eventHandlers= {this.handleLogoDrop()}/>
             </div>
             <div>
               <button type='submit'>Save</button>
