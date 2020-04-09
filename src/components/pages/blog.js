2764e3dd268798fa348c99534bd7ff42b7ff9eb1
Jyothi Turpu <jyothi.turpu@gmail.com>
2020-04-09 01:44:38 -0400

    Added getBlogItems

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-export default function Blog() {
-  return (
-    <div>
-      <h2>Blog</h2>
+export default class Blog extends Component{
 
+  constructor(props){
+    super(props);
+
+    this.state = {
+      blogItems: []
+    }
+
+    this.getBlogItems = this.getBlogItems.bind(this);
+  }
+
+  getBlogItems() {
+    axios.get('https://jyothiturpu.devcamp.space/portfolio/portfolio_blogs', { withCredentials: true }).
+    then(response => {
+      console.log('Response received in getBlogItems', response);
+      this.setState({
+        blogItems: response.data.portfolio_blogs
+      })
+    }).
+    catch(error => {
+      console.log('Error in getBlogItems', error);
+    });
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
+  }
+
+  render() {
+    return (
       <div>
-        <Link to='/about-me'>Read more about myself</Link>
+        <h2>Blog</h2>
+  
+        <div>
+          <Link to='/about-me'>Read more about myself</Link>
+        </div>
+        
       </div>
-      
-    </div>
-  )
+    )
+  }
 }
