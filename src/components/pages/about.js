b172e1d94ae1f75127c75d56bd257aa8a0330130
Jyothi Turpu <jyothi.turpu@gmail.com>
2020-04-22 17:06:43 -0400

    Implemented About&Contact Pages

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import profilePicture from '../../../static/assets/images/bio/headshot.jpg';
 
 export default function About() {
   return (
-    <div>
-      <h2>About</h2>
+    <div className='content-page-wrapper'>
+        <div className="left-column"
+          style={{
+            background: 'url(' + profilePicture + ') no-repeat',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+          }}>
+        </div>
+        <div className="right-column">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis adipisci ratione ducimus rerum, magni quis blanditiis accusantium. Nam explicabo sint iure, debitis voluptate ab architecto? Quas inventore placeat fugiat perspiciatis.
+        </div>
     </div>
   )
 }
